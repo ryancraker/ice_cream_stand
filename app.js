745db98ad1d2ac0b203c9007d6ff80fd6ef18a0c
ryancraker <ryan.craker98@gmail.com>
2025-05-27 16:41:18 -0600

    total calculations

--- app.js
+++ app.js
@@ -12,5 +12,18 @@ function buyScoop(flavorPosition) {
   console.log("added", flavorPosition, scoop);
   scoop.qty += 1;
   console.log("added to cart", scoop);
+  calculateTotal()
+}
 
+function calculateTotal() {
+  let grandTotal = 0
+
+  for (let loop = 0; loop <= 5; loop += 1) {
+    let scoop = iceCreamFlavors[loop];
+    grandTotal += scoop.price * scoop.qty
+    console.log(scoop.name, scoop.price, scoop.qty);
+
+    let totalElm = document.getElementById('total')
+    totalElm.textContent = grandTotal.toFixed(2)
+  }
 }
